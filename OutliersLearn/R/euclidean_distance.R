ee5dcc13a656d75e243a8646214b95d1f7b86ae1
MissiegoBeats <missiego.andres@gmail.com>
2024-02-09 09:23:45 +0100

    Pending to solve issue #12

--- OutliersLearn/R/euclidean_distance.R
+++ OutliersLearn/R/euclidean_distance.R
@@ -1,5 +1,5 @@
-distancia_euclidea <- function(p1,p2){
-	#distancia = sqrt((x2 - x1)^2 + (y2-y1)^2) -> aclaración de la siguiente línea
-	distancia = sqrt( (p2[1] - p1[1])^2 + (p2[2]-p1[2])^2 )
-	return(distancia)
+euclidean_distance <- function(p1,p2){
+	#distance = sqrt((x2 - x1)^2 + (y2-y1)^2) -> This is the formula of the next line of code
+	distance = sqrt( (p2[1] - p1[1])^2 + (p2[2]-p1[2])^2 )
+	return(distance)
 }
