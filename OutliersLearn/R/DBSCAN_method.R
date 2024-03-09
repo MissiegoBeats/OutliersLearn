7c3dedc3ca71cc2e6cbfed11ebb59b8185e1d17c
MissiegoBeats <missiego.andres@gmail.com>
2024-03-09 11:31:30 +0100

    Still pending to test it

--- OutliersLearn/R/DBSCAN_method.R
+++ OutliersLearn/R/DBSCAN_method.R
@@ -9,69 +9,83 @@
 #' @export
 
 DBSCAN_method <- function(inputData, max_distance_threshold, min_pts){
+  #Algorithm pseudocode:
+  #https://www.researchgate.net/figure/Pseudocode-of-DBSCAN-Algorithm-28_fig2_339165857
+
   #Conversion to matrix if it is a data frame (this is common to both options)
   #and it's not relevant for the explanation
   if (is.data.frame(inputData)) {
     inputData = as.matrix(inputData);
-
   } else {
     stop("inputData must be a dataframe")
   }
 
   #Create the necessary data structures for the algorithm
   cluster_id = 0;
-  visited_array = numeric(nrow(inputData)); #0 means not visited. 1 means visited. -1 means noise
+  visited_array = numeric(nrow(inputData)); #0 means undefined. -1 means noise. >0 means cluster id
   clusters = c();
-
   for(i in 1:nrow(inputData)){
-    if(visited_array[i] == 0){ #Only process if not visited
-      visited_array[i] = 1;
-      #Calculate the distance between this point and the rest of the points
-      distances = c();
-      neighbors = c();
-      for(j in 1:nrow(inputData)){
-        if(euclidean_distance(inputData[i,],inputData[j,]) <= max_distance_threshold){
-          neighbors = c(neighbors,j);
-        }
+
+    if(visited_array[i] == 0){
+      next;
+    }
+
+    #Calculate the distance between this point (i) and the rest of the points. This is the
+    #equivalent to the RangeQuery() functionality
+    distances = c();
+    neighbors = c();
+    for(j in 1:nrow(inputData)){
+      if(euclidean_distance(inputData[i,],inputData[j,]) <= max_distance_threshold){
+        neighbors = c(neighbors,j);
       }
-      if(length(neighbors) < min_pts){
-        visited_array[i] = -1;
-      }else{
-        cluster_id = cluster_id+1;
-        clusters[i] = cluster_id;
-        visited_array[i] = 1;
-        for(k in 1:length(neighbors)){
-          if(visited_array[k]==0){
-            visited_array[k] = 1;
+    }
+
+    if(length(neighbors) < min_pts){
+      visited_array[i] = -1;
+      next;
+    }
+
+    cluster_id = cluster_id + 1;
+    visited_array[i] = cluster_id;
+    j = 1;
+    while(j <= length(neighbors)){
+      if(j!=i){
+        if(visited_array[j] == -1){
+          visited_array[j] = cluster_id;
+        }
+        if(visited_array[j] == 0){
+          j = j+1;
+          next;
+        }
+
+        #Calculate the distance between this point (j) and the rest of the points. This is the
+        #equivalent to the RangeQuery() functionality
+        distances = c();
+        neighbors_aux = c();
+        for(k in 1:nrow(inputData)){
+          if(euclidean_distance(inputData[j,],inputData[k,]) <= max_distance_threshold){
+            neighbors_aux = c(neighbors_aux,k);
           }
         }
+
+        visited_array[j] = cluster_id;
+        if(length(neighbors) < min_pts){
+          j = j+1;
+          next;
+        }
+
+        neighbors = c(neighbors, neighbors_aux)
+        j = j+1;
       }
     }
   }
+
+  for(i in 1:length(visited_array)){
+    if(visited_array[i] == -1){
+      message(sprintf("The point %d is an outlier", i))
+    }
+  }
+
 }
 
-#Pseudocode for this algorithm:
-#DBSCAN_OutlierDetection(data, eps, minPts):
-# Initialize cluster_id to 0
-#
-# for each point p in data:
-#   if p is visited:
-#   continue
-# mark p as visited
-# neighbors = regionQuery(p, eps)
-#
-# if size(neighbors) < minPts:
-#   mark p as noise
-# else:
-#   cluster_id = cluster_id + 1
-# expandCluster(p, neighbors, cluster_id, eps, minPts)
-#
-# outliers = findNoisePoints(data)
-# return outliers
-#
-# findNoisePoints(data):
-#   outliers = empty list
-# for each point p in data:
-#   if p is noise:
-#   add p to outliers
-# return outliers
+
