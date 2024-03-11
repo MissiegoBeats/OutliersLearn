85ffbe26e3c2919fa60d6dccf784931f0d0df6ff
MissiegoBeats <missiego.andres@gmail.com>
2024-03-11 23:33:47 +0100

    DBSCAN now works, still pending to add the learning option and some visual representation if it's possible

--- OutliersLearn/R/DBSCAN_method.R
+++ OutliersLearn/R/DBSCAN_method.R
@@ -11,6 +11,7 @@
 DBSCAN_method <- function(inputData, max_distance_threshold, min_pts){
   #Algorithm pseudocode:
   #https://www.researchgate.net/figure/Pseudocode-of-DBSCAN-Algorithm-28_fig2_339165857
+  #https://cse.buffalo.edu/~jing/cse601/fa13/materials/clustering_density.pdf
 
   #Conversion to matrix if it is a data frame (this is common to both options)
   #and it's not relevant for the explanation
@@ -23,7 +24,7 @@ DBSCAN_method <- function(inputData, max_distance_threshold, min_pts){
   #Create the necessary data structures for the algorithm
   cluster_id = 0;
   visited_array = numeric(nrow(inputData)); #0 means undefined. -1 means noise. >0 means cluster id
-  clusters = c();
+  clusters = numeric(nrow(inputData));
   for(i in 1:nrow(inputData)){
     if(visited_array[i] != 0){
       next;
@@ -46,8 +47,27 @@ DBSCAN_method <- function(inputData, max_distance_threshold, min_pts){
 
     cluster_id = cluster_id + 1;
     #expandCluster
-    #TODO: implement pseudocode of this: https://cse.buffalo.edu/~jing/cse601/fa13/materials/clustering_density.pdf
-
+    #Pseudocode implementation of this: https://cse.buffalo.edu/~jing/cse601/fa13/materials/clustering_density.pdf
+    print(neighbors);
+    print(visited_array);
+    clusters[i] = cluster_id; #Add P to cluster C
+    for(j in 1:length(neighbors)){ #if neighbor is not visited
+      if(visited_array[neighbors[j]] != 0){
+        visited_array[neighbors[j]] = 1; #mark as visited
+        distances_neighbors = c();
+        neighbors_aux = c();
+        for(k in 1:nrow(inputData)){
+          if(euclidean_distance(inputData[neighbors[j],], inputData[k,]) <= max_distance_threshold){
+            neighbors_aux = c(neighbors_aux,k);
+          }
+        }
+        if(length(neighbors_aux) >= min_pts){
+          neighbors = union(neighbors, neighbors_aux);
+        }
+      }else if(clusters[neighbors[j]] != 0){
+        clusters[neighbors[j]] = cluster_id;
+      }
+    }
   }
 
   for(i in 1:length(visited_array)){
--- OutliersLearn/R/tests.R
+++ OutliersLearn/R/tests.R
@@ -2,9 +2,9 @@
 #Nota: para cuando quiera cambiar la documentación de las funciones roxygen2::roxygenize()
 inputData = t(matrix(c(3,2,3.5,12,4.7,4.1,5.2,4.9,7.1,6.1,6.2,5.2,14,5.3),2,7,dimnames=list(c("r","d"))))
 inputData = data.frame(inputData)
-print(inputData)
-dist(inputData)
-plot(inputData);
+#print(inputData)
+#dist(inputData)
+#plot(inputData);
 #inputData <- transform_to_vector(inputData)
 #print(inputData)
 #mahalanobis_method(inputData,0.2);
