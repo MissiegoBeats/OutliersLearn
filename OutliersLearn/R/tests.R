debaeda63602bb9e2a0adc314df837c20a2df78b
MissiegoBeats <missiego.andres@gmail.com>
2024-04-29 14:09:53 +0200

    updated tests.R

--- OutliersLearn/R/tests.R
+++ OutliersLearn/R/tests.R
@@ -1,7 +1,6 @@
 #TODO delete this file when everything works
 #Nota: para cuando quiera cambiar la documentación de las funciones roxygen2::roxygenize()
-inputData = t(matrix(c(3,2,3.5,12,4.7,4.1,5.2,4.9,7.1,6.1,6.2,5.2,14,5.3),2,7,dimnames=list(c("r","d"))))
-inputData = data.frame(inputData)
+
 #print(inputData)
 #dist(inputData)
 #plot(inputData);
@@ -9,4 +8,6 @@ inputData = data.frame(inputData)
 #print(inputData)
 #mahalanobis_method(inputData,0.2);
 #pca_method(inputData, TRUE);
-knn(inputData,3.5,3,FALSE);
+inputData = t(matrix(c(3,2,3.5,12,4.7,4.1,5.2,4.9,7.1,6.1,6.2,5.2,14,5.3),2,7,dimnames=list(c("r","d"))))
+inputData = data.frame(inputData)
+knn(inputData,3.5,3,TRUE);
