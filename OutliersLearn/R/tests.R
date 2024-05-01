fcfc7c51b51bbfac0c0597745a4cd42109190e22
MissiegoBeats <missiego.andres@gmail.com>
2024-05-01 23:22:07 +0200

    LOF algorithm is not working correctly, i think it is the last step...

--- OutliersLearn/R/tests.R
+++ OutliersLearn/R/tests.R
@@ -8,6 +8,8 @@
 #print(inputData)
 #knn(inputData,3.5,3,TRUE);
 #pca_method(inputData, TRUE);
+#z_score_method(inputData,2,TRUE);
+
 inputData = t(matrix(c(3,2,3.5,12,4.7,4.1,5.2,4.9,7.1,6.1,6.2,5.2,14,5.3),2,7,dimnames=list(c("r","d"))));
 inputData = data.frame(inputData);
-z_score_method(inputData,2,TRUE);
+lof(inputData, 3.5, 1.2, TRUE);
