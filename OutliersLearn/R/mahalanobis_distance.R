70c651174b163f188d84b38ec5830fcdb145b4c1
MissiegoBeats <missiego.andres@gmail.com>
2024-05-14 19:43:50 +0200

    Nueva versión mahalanobis method

--- OutliersLearn/R/mahalanobis_distance.R
+++ OutliersLearn/R/mahalanobis_distance.R
@@ -11,12 +11,19 @@
 #'@examples
 #'
 #'@export
-mahalanobis_distance <- function(value, sample_mean, sample_covariance_matrix){
-  #See https://repository.upb.edu.co/bitstream/handle/20.500.11912/6582/Técnicas%20para%20detección%20de%20outliers%20multivariantes.pdf?sequence=1&isAllowed=y
-  #For the mahalanobis_distance formula
-  #TODO: make this work
-  diff_vector = value-sample_mea;
-  transposed_diff_vector = t(diff_vector);
-  inverted_covariance_matrix = solve(sample_covariance_matrix);
-  return(sqrt(transposed_diff_vector - inverted_covariance_matrix %*% diff_vector));
+mahalanobis_distance <- function(value, sample_mean, sample_covariance_matrix) {
+  print(value);
+  # Calculate the difference vector
+  diff_vector <- value - sample_mean
+
+  # Calculate the transposed difference vector
+  transposed_diff_vector <- t(diff_vector)
+
+  # Calculate the inverse of the covariance matrix
+  inverted_covariance_matrix <- solve(sample_covariance_matrix)
+
+  # Calculate the Mahalanobis distance
+  mahalanobis_dist <- sqrt(transposed_diff_vector %*% inverted_covariance_matrix %*% diff_vector)
+
+  return(mahalanobis_dist)
 }
