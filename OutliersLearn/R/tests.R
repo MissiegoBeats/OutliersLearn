cc20ce41d995017f47b90e6600646bb039d38390
MissiegoBeats <missiego.andres@gmail.com>
2024-05-23 23:14:21 +0200

    Mahalanobis final version, documenting now

--- OutliersLearn/R/tests.R
+++ OutliersLearn/R/tests.R
@@ -11,7 +11,11 @@
 
 inputData = t(matrix(c(3,2,3.5,12,4.7,4.1,5.2,4.9,7.1,6.1,6.2,5.2,14,5.3),2,7,dimnames=list(c("r","d"))));
 inputData = data.frame(inputData);
-mahalanobis_method(inputData, 1);
+mahalanobis_method(inputData, 0.2, TRUE);
+
+inputData = t(matrix(c(3,2,3.5,12,4.7,4.1,5.2,4.9,7.1,6.1,6.2,5.2,14,5.3),2,7,dimnames=list(c("r","d"))));
+inputData = data.frame(inputData);
+mahalanobis_method(inputData, 0.7, FALSE);
 
 #lof(inputData, 2, 2, FALSE);
 #DBSCAN_method(inputData, 4, 3, TRUE);
