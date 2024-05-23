caaaa3d97d066429a9f2e1fecd43623e8f4a1c31
MissiegoBeats <missiego.andres@gmail.com>
2024-05-23 18:37:56 +0200

    Mahalanobis method finished, still pending to add the theorican and step by step explanation and the examples in the code documentation

--- OutliersLearn/R/mahalanobis_method.R
+++ OutliersLearn/R/mahalanobis_method.R
@@ -5,11 +5,10 @@
 #'@author Andres Missiego Manjon
 #'@param inputData Input Data dataset
 #'@param alpha Significance level alpha
+#'@param tutorialMode if TRUE the tutorial mode is activated (the algorithm will include an explanation detailing the theory behind the outlier detection algorithm and a step by step explanation of how is the data processed to obtain the outliers following the theory mentioned earlier)
 #'@examples
 #'
 #'@export
-
-#TODO: implement the tutorialmode version
 
 mahalanobis_method <- function(inputData, alpha){
   #Conversion to matrix if it is a data frame (this is common to both options)
@@ -19,52 +18,95 @@ mahalanobis_method <- function(inputData, alpha){
   } else {
     stop("inputData must be a dataframe")
   }
+  if(tutorialMode){
 
-  print(inputData);
+    #TODO: add theorical explanation and step by step explanation
+    #TODO: add examples to documentation
 
-  #Check alpha value:
-  if(alpha <= 1){
+    #Check alpha value:
+    if(alpha <= 1){
 
-    #Calculate the sample mean for each variable and store it in an array:
-    sampleMeans = c();
-    for(i in 1:ncol(inputData)){
-      column = inputData[,i]; #We get the column
-      calculatedMean = sum(column)/length(column); #Calculate the mean
-      sampleMeans = c(sampleMeans, calculatedMean); #Add it to the array
-    }
-
-    #Calculate the covariance matrix:
-    covariance_matrix = cov(inputData);
-    message("Cov mat");
-    print(covariance_matrix);
-
-    #Calculate the mahalanobis distance matrix:
-    mahalanobis_mat_vector = c();
-    for(i in 1:nrow(inputData)){
-      mahalanobis_mat_vector = c(mahalanobis_mat_vector, mahalanobis_distance(inputData[i,], sampleMeans, covariance_matrix));
-      print(mahalanobis_mat_vector);
-    }
-    distances = mahalanobis_mat_vector;
-    message("My mahalanobis distance vector: ");
-    print(distances);
-    distances = mahalanobis(inputData, sampleMeans, covariance_matrix);
-    message("R own function mahalanobis(): ");
-    print(distances);
-
-    #Calculate the critical value
-    num_dimensions = ncol(inputData);
-    message("Critical Value: ")
-    critical_value <- qchisq(1 - alpha, num_dimensions); #To calculate the critical value
-    print(critical_value);
-
-    for(i in 1:length(distances)){
-      if(distances[i] > critical_value){
-        message(sprintf("The observation %d is an outlier", i));
-        message(sprintf("The values of the observation are: "));
-        print(inputData[i,]);
+      #Calculate the sample mean for each variable and store it in an array:
+      sampleMeans = c();
+      for(i in 1:ncol(inputData)){
+        column = inputData[,i]; #We get the column
+        calculatedMean = sum(column)/length(column); #Calculate the mean
+        sampleMeans = c(sampleMeans, calculatedMean); #Add it to the array
       }
+
+      #Calculate the covariance matrix:
+      covariance_matrix = cov(inputData);
+      message("Cov mat");
+      print(covariance_matrix);
+
+      #Calculate the mahalanobis distance matrix:
+      mahalanobis_mat_vector = c();
+      for(i in 1:nrow(inputData)){
+        mahalanobis_mat_vector = c(mahalanobis_mat_vector, mahalanobis_distance(inputData[i,], sampleMeans, covariance_matrix));
+      }
+      distances = mahalanobis_mat_vector;
+
+      #Obtain the squared distances
+      distances_squared = distances^2;
+
+      #Calculate the critical value
+      num_dimensions = ncol(inputData);
+      message("Critical Value: ")
+      critical_value <- qchisq(1 - alpha, num_dimensions); #To calculate the critical value
+      print(critical_value);
+
+      for(i in 1:length(distances)){
+        if(distances[i] > critical_value){
+          message(sprintf("The observation %d is an outlier", i));
+          message(sprintf("The values of the observation are: "));
+          print(inputData[i,]);
+        }
+      }
+    }else{
+      stop("Alpha can't be higher than 1");
     }
   }else{
-    stop("Alpha can't be higher than 1");
+    #Check alpha value:
+    if(alpha <= 1){
+
+      #Calculate the sample mean for each variable and store it in an array:
+      sampleMeans = c();
+      for(i in 1:ncol(inputData)){
+        column = inputData[,i]; #We get the column
+        calculatedMean = sum(column)/length(column); #Calculate the mean
+        sampleMeans = c(sampleMeans, calculatedMean); #Add it to the array
+      }
+
+      #Calculate the covariance matrix:
+      covariance_matrix = cov(inputData);
+      message("Cov mat");
+      print(covariance_matrix);
+
+      #Calculate the mahalanobis distance matrix:
+      mahalanobis_mat_vector = c();
+      for(i in 1:nrow(inputData)){
+        mahalanobis_mat_vector = c(mahalanobis_mat_vector, mahalanobis_distance(inputData[i,], sampleMeans, covariance_matrix));
+      }
+      distances = mahalanobis_mat_vector;
+
+      #Obtain the squared distances
+      distances_squared = distances^2;
+
+      #Calculate the critical value
+      num_dimensions = ncol(inputData);
+      message("Critical Value: ")
+      critical_value <- qchisq(1 - alpha, num_dimensions); #To calculate the critical value
+      print(critical_value);
+
+      for(i in 1:length(distances)){
+        if(distances[i] > critical_value){
+          message(sprintf("The observation %d is an outlier", i));
+          message(sprintf("The values of the observation are: "));
+          print(inputData[i,]);
+        }
+      }
+    }else{
+      stop("Alpha can't be higher than 1");
+    }
   }
 }
